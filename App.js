476099104b854813f2b57f85215290ff2cc7572b
Sally Dabbah <sallydabbah@gmail.com>
2019-05-17 00:30:29 +0300

    Update App.js
    added browser routing - sally

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import NavBarComponent from './NavBarComponent';
 import HomeComponent from './HomeComponent';
@@ -13,40 +14,46 @@ import LoginComponent from './LoginComponent';
 import MyEventsComponent from './MyEventsComponent';
 import SearchedEventComponent from './SearchedEventComponent';
 export default class App extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        if(!localStorage.users){
+        if (!localStorage.users) {
             localStorage.users = JSON.stringify([
-                {   
-                    "name":"ameer",
+                {
+                    "name": "ameer",
                     "userName": "ameer.outlook.com",
                     "password": 12345
                 },
-                { 
-                    "name":"saeed",
+                {
+                    "name": "saeed",
                     "userName": "saeednamih@gmail.com",
                     "password": 5678
                 },
                 {
-                    "name":"sally",
+                    "name": "sally",
                     "userName": "sallydabbah@gmail.com",
                     "password": 9101112
                 },
                 {
-                    "name":"ameer",
+                    "name": "ameer",
                     "userName": "ameer_z_90@hotmail.com",
                     "password": 131415
                 }
-               ]);
+            ]);
         }
     }
-    render(){
-        return(
+    render() {
+        return (
             <>
-             <HomeComponent/>
-            </>
-           
-        );
+                <BrowserRouter>
+                    <div>
+                        <NavBarComponent />
+                        <Switch>
+                            <Route path="/" component={HomeComponent} exact />
+                            <Route path="/events" component={EventsComponent} />
+                            <Route path="/wishes" component={WishesComponent} />
+                        </Switch>
+                    </div>
+                </BrowserRouter> </>);
     }
 }
-ReactDOM.render(<App />,document.querySelector('#container'));
+ReactDOM.render(<App />, document.querySelector('#container'));
